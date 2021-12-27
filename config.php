1de53184b3d79cc28871a6e9e069917d3acd9ad1
DANIELA MORA <DANIELA.MORA@SEGUROSNOVUS.COM.CO>
2021-12-27 12:08:10 -0500

    nombre proyecto para rutas

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
-	define("FOLD_PROY", $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/LOGIN-FORM/");
+	define("FOLD_PROY", $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/Proyecto-Veterinaria/");
 	define("FOLDER_TEMPLATE", FOLD_PROY."template/");
 	define("FOLDER_VIEWS", FOLD_PROY."app/View/");
-	define("URL_PROY", "/LOGIN-FORM/");
+	define("URL_PROY", "/Proyecto-Veterinaria/");
 	define('URL_CSS', URL_PROY."assets/css/");
 	define('URL_IMG', URL_PROY."assets/img/");
 	define('URL_VIEW', URL_PROY."app/View/");
